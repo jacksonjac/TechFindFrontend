627aba03150656fc714d4bea5fd9bf08e5ac1a0b
jackson R <jacksonjack333r@gmail.com>
2024-08-26 14:09:28 +0530

    polifillsupdate

---
+++ src/polyfills.ts
@@ -0,0 +1,16 @@
+// This file includes polyfills needed by Angular and is loaded before the app.
+// You can add your own polyfills here.
+
+// IE9, IE10 and IE11 require all of the following polyfills.
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/array';
+import 'core-js/es/string';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/promise';
+import 'core-js/es/weak-map';
+import 'core-js/es/weak-set';
+import 'core-js/es/reflect';
+import 'zone.js';
